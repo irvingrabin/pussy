606a9e12858e34af4633a5c719fab7de02d9fe20
Irving Rabin <ii6rr5@gmail.com>
2018-12-13 12:33:19 -0800

    Using type classes with tests

--- src/main/scala/pack01/Adder.scala
+++
@@ -1,5 +0,0 @@
-package pack01
-
-object Adder {
-  def add(x: Int, y: Int): Int = x + y
-}
--- src/test/scala/book/ch01/JsonInterfaceSyntaxSpec.scala
+++ src/test/scala/book/ch01/JsonInterfaceSyntaxSpec.scala
@@ -2,12 +2,13 @@ package book.ch01
 
 import org.scalatest.{FlatSpec, Matchers}
 import JsonWriterInstances._
+import JsonSyntax._
 
-class JsonInterfaceObjectsSpec extends FlatSpec with Matchers {
+class JsonInterfaceSyntaxSpec extends FlatSpec with Matchers {
   it should "produce Json from interface objects" in {
-    Json.toJson("abc") shouldBe JsonString("abc")
-    Json.toJson(14.5) shouldBe JsonDouble(14.5)
-    Json.toJson(Person("Irving Rabin", "ii6rr5@gmail.com")) shouldBe JsonObject(Map(
+    "abc".toJson shouldBe  JsonString("abc")
+    14.5.toJson shouldBe JsonDouble(14.5)
+    Person("Irving Rabin", "ii6rr5@gmail.com").toJson shouldBe JsonObject(Map(
       "name"  -> JsonString("Irving Rabin"),
       "email" -> JsonString("ii6rr5@gmail.com")
     ))
