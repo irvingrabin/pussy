600d45ba8e23708cbb9e5004cefa9a350e269dea
Irving Rabin <ii6rr5@gmail.com>
2018-12-14 02:06:25 -0500

    FixingCatsShow

--- src/main/scala/book/ch01/Cat.scala
+++ src/main/scala/book/ch01/Cat.scala
@@ -1,3 +1,14 @@
 package book.ch01
 
+import cats.Show
+import cats.instances.int._
+import cats.instances.string._
+import cats.syntax.show._
+
 final case class Cat(name: String, age: Int, color: String)
+
+object Cat {
+  implicit val catShow: Show[Cat] = Show.show[Cat] { cat =>
+    s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat"
+  }
+}
--- src/test/scala/book/ch01/ShowSpec.scala
+++ src/test/scala/book/ch01/ShowSpec.scala
@@ -1,10 +1,11 @@
 package book.ch01
 
+import java.util.Date
+
 import org.scalatest.{FlatSpec, Matchers}
 import cats.Show
 import cats.instances.int._
 import cats.instances.string._
-
 import cats.syntax.show._
 
 class ShowSpec extends FlatSpec with Matchers {
@@ -20,5 +21,20 @@ class ShowSpec extends FlatSpec with Matchers {
   it should "show cats with syntax" in {
     123.show shouldBe "123"
     "abc".show shouldBe "abc"
+  }
+
+  it should "show custom instances 1" in {
+    implicit val dateShow: Show[Date] = new Show[Date] {
+      def show(date: Date): String = s"${date.getTime} since the epoch"
+    }
+  }
+
+  it should "show custom instances 2" in {
+    implicit val dateShow: Show[Date] = Show.show(
+      date => s"${date.getTime} since the epoch")
+  }
+
+  it should "show custom instances defined correctly" in {
+    Cat("Bars", 5, "black").show shouldBe "Bars is a 5 year-old black cat"
   }
 }
